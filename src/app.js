40858833e93d360717fe6d93dc8471087eb94fc8
maxi-gartner <gartner.maxi@hotmail.com>
2024-11-09 19:50:55 -0300

    9/11

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-import handleErrors from "./utils/handleErrors.js";
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import { connectDB } from "./config/db.js"
+import handleErrors from "./utils/handleErrors.js"
 
-import indexRouter from "./routes/indexRouter.js";
+import indexRouter from "./routes/indexRouter.js"
 
-dotenv.config();
-connectDB();
-const server = express();
-server.use(express.json());
-server.use(cors());
+dotenv.config()
+connectDB()
+const server = express()
+server.use(express.json())
+server.use(cors())
 
-server.use("/api", indexRouter);
+server.use("/api", indexRouter)
 server.get("/", (req, res) => {
-  res.send("Welcome!");
-});
+  res.send("Welcome!!")
+})
 
-server.use(handleErrors);
+server.use(handleErrors)
 
 server.listen(process.env.PORT, () =>
   console.log("Listening on port", process.env.PORT)
-);
+)
